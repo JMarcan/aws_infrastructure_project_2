41aae5aa1972b6f9e7c2db36edb5f0309c815f75
JMarcan <jan.marcan@protonmail.com>
2021-02-01 22:19:53 +0100

    feat: move infrastructure to region us-east-1

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,7 @@
 # Designate a cloud provider, region, and credentials
 provider "aws" {
     profile = "default"
-    region = "eu-central-1"
+    region = "us-east-1"
 }
 
 # provision 4 AWS t2.micro EC2 instances named Udacity T2
